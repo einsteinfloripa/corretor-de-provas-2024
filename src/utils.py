97b68f5132d79a74f53fec12690c273b95ad06fe
Gustavo Bernardo Rodrigues <gurodrigues.b@gmail.com>
2024-07-24 15:49:04 -0300

    feat: create utils file

---
+++ src/utils.py
@@ -0,0 +1,38 @@
+textColor = {
+    'matematica': 'green',
+    'portugues': '#B8AA5E',
+    'fisica': 'red',
+    'quimica': 'blue',
+    'historia': 'orange',
+    'biologia': 'purple',
+    'geografia': 'cyan',
+    'ingles': 'pink',
+    'interdisciplinar': 'brown',
+    'filosofia-sociologia': 'gray'  
+}
+
+mediaDeAcertos = {
+    'matematica': 'green',
+    'portugues': '#B8AA5E',
+    'fisica': 'red',
+    'quimica': 'blue',
+    'historia': 'orange',
+    'biologia': 'purple',
+    'geografia': 'cyan',
+    'ingles': 'pink',
+    'interdisciplinar': 'brown',
+    'filosofia-sociologia': 'gray'  
+}
+
+textCorrector = {
+    'matematica': 'Matemática',
+    'portugues': 'Português',
+    'fisica': 'Física',
+    'quimica': 'Química',
+    'historia': 'História',
+    'biologia': 'Biologia', 
+    'geografia': 'Geografia', 
+    'ingles': 'Inglês',
+    'interdisciplinar': 'Interdisciplinar',
+    'filosofia-sociologia': 'Filosofia-Sociologia'  
+}
