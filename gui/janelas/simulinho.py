0ed4ee92cc9541aef4ee81ef2a67262d3d5c5f70
Gustavo Bernardo Rodrigues <gurodrigues.b@gmail.com>
2024-07-25 17:40:30 -0300

    refactor: change width and height of window

--- gui/janelas/simulinho.py
+++ gui/janelas/simulinho.py
@@ -1,5 +1,4 @@
 from PyQt5.QtWidgets import QMainWindow, QPushButton, QLabel
-
 class SimulinhoJanela(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -7,12 +6,12 @@ class SimulinhoJanela(QMainWindow):
         self.topo = 200
         self.lado = 550
         self.largura = 800
-        self.altura = 800
+        self.altura = 600
         self.titulo = "Corretor de Simulados"
 
         botaoGerarVariaveis = QPushButton("GerarVariaveis", self)
-        botaoGerarVariaveis.move(70,450)
-        botaoGerarVariaveis.resize(300,100)
+        botaoGerarVariaveis.move(170,450)
+        botaoGerarVariaveis.resize(200,60)
         botaoGerarVariaveis.setStyleSheet('''
             QPushButton {
                 background-color: #362500;
@@ -31,8 +30,8 @@ class SimulinhoJanela(QMainWindow):
         # botaoGerarVariaveis.clicked.connect(self.Simulinho)
 
         botaoGerarResultados = QPushButton("GerarResultados", self)
-        botaoGerarResultados.move(400,450)
-        botaoGerarResultados.resize(300,100)
+        botaoGerarResultados.move(450,450)
+        botaoGerarResultados.resize(200,60)
         botaoGerarResultados.setStyleSheet('''
             QPushButton {
                 background-color: #493b72;
@@ -50,14 +49,13 @@ class SimulinhoJanela(QMainWindow):
 
         tituloTopo = QLabel(self)
         tituloTopo.setText("Simulinho")
-        tituloTopo.setGeometry(300, 20, 450, 100)  # Ajusta o tamanho e a posição do QLabel
+        tituloTopo.setGeometry(320, 20, 450, 100)  # Ajusta o tamanho e a posição do QLabel. Pode ser usado o resize tambem
         tituloTopo.setStyleSheet(
             '''
-            QLabel {font:bold;font-size:40px}
+            QLabel {font-size:32px;font-family:Cyrillic}
             
             ''')
 
-        self.CarregarJanela()
 
     def CarregarJanela(self):
         self.setGeometry(self.lado, self.topo, self.largura, self.altura)
