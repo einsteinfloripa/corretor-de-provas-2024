2164ad2a24b04e81ee968f0042af5fb5518e3d56
Gustavo Bernardo Rodrigues <gurodrigues.b@gmail.com>
2024-07-25 18:30:16 -0300

    chore: dynamic variables for styles

--- gui/janelas/utiils.py
+++ gui/janelas/utiils.py
@@ -14,4 +14,85 @@ def configurar_label_com_imagem(label: QLabel, file_path: str, width: int, heigh
     # Configura o tamanho e a posição do QLabel
     label.setGeometry(x, y, width, height)
 
+SimulinhoBotaoStyle = '''
+            QPushButton {
+                background-color: #362500;
+                border-radius: 6px;
+                color: #e7e6e3;
+                font-size: 20px;
+            }
+            QPushButton:hover {
+                background-color: #5a4d3d;
+            }
+            QPushButton:pressed {
+                background-color: #f6f7fa;
+            }
+        '''
 
+EnemBotaoStyle = '''
+            QPushButton {
+                background-color: #493b72;
+                border-radius: 6px;
+                color: #e7e6e3;
+                font-size: 20px;
+            }
+            QPushButton:hover {
+                background-color: #5a4d3d;
+            }
+            QPushButton:pressed {
+                background-color: #f6f7fa;
+            }
+        '''
+
+UfscBotaoStyle = '''
+            QPushButton {
+                background-color: #00422b;
+                border-radius: 6px;
+                color: #e7e6e3;
+                font-size: 20px;
+            }
+            QPushButton:hover {
+                background-color: #5a4d3d;
+            }
+            QPushButton:pressed {
+                background-color: #f6f7fa;
+            }
+        '''
+
+UdescBotaoStyle = '''
+            QPushButton {
+                background-color: #924800;
+                border-radius: 6px;
+                color: #e7e6e3;
+                font-size: 20px;
+            }
+            QPushButton:hover {
+                background-color: #5a4d3d;
+            }
+            QPushButton:pressed {
+                background-color: #f6f7fa;
+            }
+        '''
+
+PsBotaoStyle = '''
+            QPushButton {
+                background-color: #121e27;
+                border-radius: 6px;
+                color: #e7e6e3;
+                font-size: 20px;
+            }
+            QPushButton:hover {
+                background-color: #5a4d3d;
+            }
+            QPushButton:pressed {
+                background-color: #f6f7fa;
+            }
+        '''
+
+tituloTopoStyle = '''
+            QLabel {
+                font-family: "Cyrillic";   
+                font-size: 32px;      
+                background-color: transparent;
+            }
+        ''' 
--- main_window.py
+++ main_window.py
@@ -4,7 +4,7 @@ from src.alternatives.ranking import gerar_ranking
 from gui.janelas.simulinho import SimulinhoJanela
 from PyQt5.QtGui import QPixmap, QImageReader
 from PyQt5.QtCore import Qt
-from gui.janelas.utiils import configurar_label_com_imagem
+from gui.janelas.utiils import configurar_label_com_imagem, EnemBotaoStyle, UfscBotaoStyle, SimulinhoBotaoStyle, UdescBotaoStyle, PsBotaoStyle, tituloTopoStyle
 
     
 
@@ -21,104 +21,33 @@ class Janela(QMainWindow):
         botaoSimulinho = QPushButton("Simulinho", self)
         botaoSimulinho.move(290,150) #posicao do botao = x,y
         botaoSimulinho.resize(200,60) #largura, altura
-        botaoSimulinho.setStyleSheet('''
-            QPushButton {
-                background-color: #362500;
-                border-radius: 6px;
-                color: #e7e6e3;
-                font-size: 20px;
-            }
-            QPushButton:hover {
-                background-color: #5a4d3d;
-            }
-            QPushButton:pressed {
-                background-color: #f6f7fa;
-            }
-        ''')
+        botaoSimulinho.setStyleSheet(SimulinhoBotaoStyle)
         botaoSimulinho.clicked.connect(self.abrir_simulinho)
 
         botaoEnem = QPushButton("Enem", self)
         botaoEnem.move(290,220) #posicao do botao = x,y
         botaoEnem.resize(200,60) #largura, altura
-        botaoEnem.setStyleSheet('''
-            QPushButton {
-                background-color: #493b72;
-                border-radius: 6px;
-                color: #e7e6e3;
-                font-size: 20px;
-            }
-            QPushButton:hover {
-                background-color: #5a4d3d;
-            }
-            QPushButton:pressed {
-                background-color: #f6f7fa;
-            }
-        ''')
+        botaoEnem.setStyleSheet(EnemBotaoStyle)
 
         botaoUfsc = QPushButton("Ufsc", self)
         botaoUfsc.move(290,290) #posicao do botao = x,y
         botaoUfsc.resize(200,60) #largura, altura
-        botaoUfsc.setStyleSheet('''
-            QPushButton {
-                background-color: #00422b;
-                border-radius: 6px;
-                color: #e7e6e3;
-                font-size: 20px;
-            }
-            QPushButton:hover {
-                background-color: #5a4d3d;
-            }
-            QPushButton:pressed {
-                background-color: #f6f7fa;
-            }
-        ''')
+        botaoUfsc.setStyleSheet(UfscBotaoStyle)
 
         botaoUdesc = QPushButton("Udesc", self)
         botaoUdesc.move(290,360) #posicao do botao = x,y
         botaoUdesc.resize(200,60) #largura, altura
-        botaoUdesc.setStyleSheet('''
-            QPushButton {
-                background-color: #924800;
-                border-radius: 6px;
-                color: #e7e6e3;
-                font-size: 20px;
-            }
-            QPushButton:hover {
-                background-color: #5a4d3d;
-            }
-            QPushButton:pressed {
-                background-color: #f6f7fa;
-            }
-        ''')
+        botaoUdesc.setStyleSheet(UdescBotaoStyle)
 
         botaoPs = QPushButton("Ps", self)
         botaoPs.move(290,430) #posicao do botao = x,y
         botaoPs.resize(200,60) #largura, altura
-        botaoPs.setStyleSheet('''
-            QPushButton {
-                background-color: #121e27;
-                border-radius: 6px;
-                color: #e7e6e3;
-                font-size: 20px;
-            }
-            QPushButton:hover {
-                background-color: #5a4d3d;
-            }
-            QPushButton:pressed {
-                background-color: #f6f7fa;
-            }
-        ''')
+        botaoPs.setStyleSheet(PsBotaoStyle)
 
         tituloTopo = QLabel(self)
         tituloTopo.setText("Corretor de simulados")
         tituloTopo.setGeometry(233, 20, 450, 100)  # Ajusta o tamanho e a posição do QLabel
-        tituloTopo.setStyleSheet('''
-            QLabel {
-                font-family: "Cyrillic";   
-                font-size: 32px;      
-                background-color: transparent;
-            }
-        ''')
+        tituloTopo.setStyleSheet(tituloTopoStyle)
 
         self.icon = QLabel(self)
         configurar_label_com_imagem(self.icon, "./assets/icons/einsteinlogo.png", 100, 100, 30, 470)
