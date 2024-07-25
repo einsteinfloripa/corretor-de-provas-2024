94ef76e70795379b8bbdb1b84776979861bcca74
Gustavo Bernardo Rodrigues <gurodrigues.b@gmail.com>
2024-07-25 01:39:15 -0300

    refactor: create a function to colect students info

--- src/colect-infos.py
+++ src/colect-infos.py
@@ -1,16 +1,13 @@
 import pandas as pd
 
-infos = './assets/alunos_infos_2024 - Respostas ao formulário 1.csv'
+infos = './assets/spreadsheets/alunos_infos.csv'
 df_infos = pd.read_csv(infos,dtype={'CPF': str})
 linhas = df_infos.shape[0]
 
 #dicionario cpf:nome
 alunos_infos = {}
-for l in range(linhas):
-    cpf = df_infos.iloc[l,1].replace('-','').replace('.','').replace(' ','').replace("'","")
-    alunos_infos[cpf] = df_infos.iloc[l,0]
-
-def printDict():
-    for aluno in alunos_infos:
-        print(aluno,':',alunos_infos[aluno])
+def generate_info():
+    for l in range(linhas):
+        cpf = df_infos.iloc[l,1].replace('-','').replace('.','').replace(' ','').replace("'","")
+        alunos_infos[cpf] = df_infos.iloc[l,0]
 
