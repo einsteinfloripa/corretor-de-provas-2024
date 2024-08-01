5d1ca14077a9a05ba0b249b790089a51d4b61e04
Gustavo Bernardo Rodrigues <gurodrigues.b@gmail.com>
2024-08-01 11:02:05 -0300

    feat: add condition to converter str to int

--- src/ufsc/calculator.py
+++ src/ufsc/calculator.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import json
-from time import sleep
+
 df_ufsc = pd.read_csv("./assets/spreadsheets/Cópia de SimuUFSC 2024 - Dados brutos.xlsx - Sheet1.csv")
 total_de_linhas = df_ufsc.shape[0]
 
@@ -17,6 +17,16 @@ for index, row in df_ufsc.iterrows():#para cada linha, ele me da todas as coluna
     for questao in range(1,41): #as questoes vão de 1 a 40
         questao = str(questao)
         resposta = (row[questao])
+        print(isinstance(resposta,str),resposta)
+        if isinstance(resposta,str):
+            if resposta== "0" or resposta=="-" or resposta=="--":
+                resposta = 0
+            elif resposta[0] == "0" and len(resposta)>1:
+                resposta = resposta[1::]
+                resposta = int(resposta)
+            else:
+                resposta = int(resposta)
+
         respostas[cpf]["respostas_do_aluno"][questao] = resposta
 
 with open('./output/json/respostas_ufsc.json', 'w') as f:
