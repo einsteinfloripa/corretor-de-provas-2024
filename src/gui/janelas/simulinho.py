906f6cfb1326c3c200d85246892a1da7b433b244
Gustavo Bernardo Rodrigues <gurodrigues.b@gmail.com>
2024-08-26 10:43:59 -0300

    fix: change folder to generate the executable (2)

---
+++ src/gui/janelas/simulinho.py
@@ -0,0 +1,99 @@
+from PyQt5.QtWidgets import QMainWindow, QPushButton, QLabel, QVBoxLayout, QWidget, QFileDialog
+from PyQt5 import QtGui
+from .utiils import configurar_label_com_imagem, botaoGerarResultadosStyle, tituloTopoStyle, SelecionarDadosBrutosStyle, SelecionarInformaçõesStyle, SelecionarCaminhoStyle
+from src.alternatives.alternatives import gerar_grafico_distruibuicao
+
+class SimulinhoJanela(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setStyleSheet("background-color: #362500;") 
+        self.topo = 200
+        self.lado = 550
+        self.largura = 800
+        self.altura = 600
+        self.titulo = "Corretor de Simulados"
+        self.CaminhosDeArquivos = {
+            "dados_brutos" : "",
+            "infos_alunos":"",
+            "salvar_arquivos":""
+        }
+        botaoGerarVariaveis = QPushButton("GerarVariaveis", self)
+        botaoGerarVariaveis.move(170,450)
+        botaoGerarVariaveis.resize(200,60)
+        botaoGerarVariaveis.setStyleSheet(botaoGerarResultadosStyle)
+        botaoGerarVariaveis.clicked.connect(lambda:gerar_grafico_distruibuicao("simulinho",self.CaminhosDeArquivos["dados_brutos"],self.CaminhosDeArquivos["salvar_arquivos"]))
+        botaoGerarVariaveis.clicked.connect(self.textoDeStatus)
+
+        botaoGerarResultados = QPushButton("GerarResultados", self)
+        botaoGerarResultados.move(450,450)
+        botaoGerarResultados.resize(200,60)
+        botaoGerarResultados.setStyleSheet(botaoGerarResultadosStyle)
+
+        tituloTopo = QLabel(self)
+        tituloTopo.setText("Simulinho")
+        tituloTopo.setGeometry(320, 20, 450, 100)  # x,y,width, height
+        tituloTopo.setStyleSheet(tituloTopoStyle)
+
+
+        self.icon = QLabel(self)
+        configurar_label_com_imagem(self.icon, "./assets/icons/einsteinlogo.png", 100, 100, 30, 470)
+    
+
+        # Criação do QPushButton
+        self.SelecionarDadosBrutos = QPushButton("Selecionar dados brutos", self)
+        self.SelecionarDadosBrutos.setObjectName("dados_brutos")
+        self.SelecionarDadosBrutos.move(290,150)
+        self.SelecionarDadosBrutos.resize(200,65)
+        self.SelecionarDadosBrutos.setStyleSheet(SelecionarDadosBrutosStyle)
+        self.SelecionarDadosBrutos.clicked.connect(self.open_file_dialog)
+
+        self.SelecionarInformações = QPushButton("Selecionar infos alunos", self)
+        self.SelecionarInformações.setObjectName("infos_alunos")
+        self.SelecionarInformações.move(290,250)
+        self.SelecionarInformações.resize(200,65)
+        self.SelecionarInformações.setStyleSheet(SelecionarInformaçõesStyle)
+        self.SelecionarInformações.clicked.connect(self.open_file_dialog)
+
+        self.SelecionarCaminho = QPushButton("Caminho dos arquivos", self)
+        self.SelecionarCaminho.setObjectName("salvar_arquivos")
+        self.SelecionarCaminho.move(290,350)
+        self.SelecionarCaminho.resize(200,65)
+        self.SelecionarCaminho.setStyleSheet(SelecionarCaminhoStyle)
+        self.SelecionarCaminho.clicked.connect(self.openDirectory)
+
+        self.CarregarJanela()
+
+    def CarregarJanela(self):
+        self.setGeometry(self.lado, self.topo, self.largura, self.altura)
+        self.setWindowTitle(self.titulo)
+        self.show()
+
+    def open_file_dialog(self):
+        options = QFileDialog.Options()
+        file_name, _ = QFileDialog.getOpenFileName(self, "Selecionar Arquivo", "", "Todos os Arquivos (*);;Arquivos de Texto (*.txt)", options=options)
+        # Obtém o botão que enviou o sinal
+        button = self.sender()
+        print(file_name)
+        if file_name:
+            self.CaminhosDeArquivos[button.objectName()] = file_name
+            button.setText(f"Arquivo selecionado: {file_name}")
+            print(self.CaminhosDeArquivos)
+    
+    def openDirectory(self):
+        options = QFileDialog.Options()
+        folder = QFileDialog.getExistingDirectory(self, "Selecione o Diretório", "", options=options)
+        button = self.sender()
+        if folder:
+            self.CaminhosDeArquivos[button.objectName()] = folder
+            button.setText(f"Arquivo selecionado: {folder}")
+            print(f"Diretório selecionado: {folder}")
+            # Aqui você pode adicionar lógica para salvar o caminho selecionado para uso futuro
+
+    
+    def textoDeStatus(self):
+        textoDeStatus = QLabel(self)
+        textoDeStatus.setText("Variaveis Criadas!")
+        textoDeStatus.setGeometry(220, 450, 450, 100)  # x,y,width, height
+        textoDeStatus.setStyleSheet(tituloTopoStyle)
+
+
---
+++ src/gui/janelas/utiils.py
@@ -0,0 +1,295 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QWidget
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+
+def configurar_label_com_imagem(label: QLabel, file_path: str, width: int, height: int, x: int, y: int):
+    # Carrega e redimensiona o QPixmap
+    pixmap = QPixmap(file_path)
+    scaled_pixmap = pixmap.scaled(width, height, Qt.KeepAspectRatio, Qt.SmoothTransformation)
+    
+    # Configura o QPixmap no QLabel
+    label.setPixmap(scaled_pixmap)
+    
+    # Configura o tamanho e a posição do QLabel
+    label.setGeometry(x, y, width, height)
+
+SimulinhoBotaoStyle = '''
+            QPushButton {
+                background-color: #362500;
+                border-radius: 6px;
+                color: #e7e6e3;
+                font-size: 20px;
+            }
+            QPushButton:hover {
+                background-color: #5a4d3d;
+            }
+            QPushButton:pressed {
+                background-color: #f6f7fa;
+            }
+        '''
+
+EnemBotaoStyle = '''
+            QPushButton {
+                background-color: #493b72;
+                border-radius: 6px;
+                color: #e7e6e3;
+                font-size: 20px;
+            }
+            QPushButton:hover {
+                background-color: #5a4d3d;
+            }
+            QPushButton:pressed {
+                background-color: #f6f7fa;
+            }
+        '''
+
+UfscBotaoStyle = '''
+            QPushButton {
+                background-color: #00422b;
+                border-radius: 6px;
+                color: #e7e6e3;
+                font-size: 20px;
+            }
+            QPushButton:hover {
+                background-color: #5a4d3d;
+            }
+            QPushButton:pressed {
+                background-color: #f6f7fa;
+            }
+        '''
+
+UdescBotaoStyle = '''
+            QPushButton {
+                background-color: #924800;
+                border-radius: 6px;
+                color: #e7e6e3;
+                font-size: 20px;
+            }
+            QPushButton:hover {
+                background-color: #5a4d3d;
+            }
+            QPushButton:pressed {
+                background-color: #f6f7fa;
+            }
+        '''
+
+PsBotaoStyle = '''
+            QPushButton {
+                background-color: #121e27;
+                border-radius: 6px;
+                color: #e7e6e3;
+                font-size: 20px;
+            }
+            QPushButton:hover {
+                background-color: #5a4d3d;
+            }
+            QPushButton:pressed {
+                background-color: #f6f7fa;
+            }
+        '''
+
+tituloTopoStyle = '''
+            QLabel {
+                font-family: "Cyrillic";   
+                font-size: 32px;      
+                background-color: transparent;
+            }
+        '''
+
+dadosBrutosArquivoStyle = '''
+        QLabel {
+                text-align:center;
+                font-family:"Cyrillic";   
+                font-size:15px;
+                color:black;  
+                background-color:#239B56 ;
+                border-radius:6px;
+            }
+
+'''
+
+
+
+
+botaoGerarResultadosStyle = '''
+            QPushButton {
+                background-color: #493b72;
+                border-radius: 6px;
+                color: #e7e6e3;
+                font-size: 20px;
+            }
+            QPushButton:hover {
+                background-color: #5a4d3d;
+            }
+            QPushButton:pressed {
+                background-color: #f6f7fa;
+            }
+        ''' 
+
+SelecionarDadosBrutosStyle= '''
+            QPushButton {
+                background-color: #00422b;
+                border-radius: 6px;
+                color: #e7e6e3;
+                font-size:15px;
+            }
+            QPushButton:hover {
+                background-color: #00734a;
+            }
+            QPushButton:pressed {
+                background-color: #f6f7fa;
+            }
+        '''
+
+SelecionarInformaçõesStyle = '''
+        QPushButton {
+                background-color: #00422b;
+                border-radius: 6px;
+                color: #e7e6e3;
+                font-size:15px;
+            }
+            QPushButton:hover {
+                background-color: #00734a;
+            }
+            QPushButton:pressed {
+                background-color: #f6f7fa;
+            }
+        '''
+
+
+SelecionarCaminhoStyle ='''
+            QPushButton {
+                background-color: #00422b;
+                border-radius: 6px;
+                color: #e7e6e3;
+                font-size:15px;
+            }
+            QPushButton:hover {
+                background-color: #117A65;
+            }
+            QPushButton:pressed {
+                background-color: #f6f7fa;
+            }
+        '''
+
+
+SelecionarDadosBrutosUfscStyle= '''
+            QPushButton {
+                background-color: #ee7867;
+                border-radius: 6px;
+                color: #e7e6e3;
+                font-size:15px;
+            }
+            QPushButton:hover {
+                background-color: #00734a;
+            }
+            QPushButton:pressed {
+                background-color: #f6f7fa;
+            }
+        '''
+
+SelecionarCaminhoUfscStyle= '''
+            QPushButton {
+                background-color: #ee7867;
+                border-radius: 6px;
+                color: #e7e6e3;
+                font-size:15px;
+            }
+            QPushButton:hover {
+                background-color: #00734a;
+            }
+            QPushButton:pressed {
+                background-color: #f6f7fa;
+            }
+        '''
+
+SelecionarValeUfscGabaritoStyle= '''
+            QPushButton {
+                background-color: #ee7867;
+                border-radius: 6px;
+                color: #e7e6e3;
+                font-size:15px;
+            }
+            QPushButton:hover {
+                background-color: #00734a;
+            }
+            QPushButton:pressed {
+                background-color: #f6f7fa;
+            }
+        '''
+
+SelecionarCaminhoUfscStyle= '''
+            QPushButton {
+                background-color: #ee7867;
+                border-radius: 6px;
+                color: #e7e6e3;
+                font-size:15px;
+            }
+            QPushButton:hover {
+                background-color: #00734a;
+            }
+            QPushButton:pressed {
+                background-color: #f6f7fa;
+            }
+        '''
+
+SelecionarInformaçõesUfscStyle = '''
+            QPushButton {
+                background-color: #ee7867;
+                border-radius: 6px;
+                color: #e7e6e3;
+                font-size:15px;
+            }
+            QPushButton:hover {
+                background-color: #00734a;
+            }
+            QPushButton:pressed {
+                background-color: #f6f7fa;
+            }
+        '''
+
+SelecionarCaminhoUfscRelatórioAlunosStyle = '''
+            QPushButton {
+                background-color: #ee7867;
+                border-radius: 6px;
+                color: #e7e6e3;
+                font-size:15px;
+            }
+            QPushButton:hover {
+                background-color: #00734a;
+            }
+            QPushButton:pressed {
+                background-color: #f6f7fa;
+            }
+        '''
+
+como_usar_link = "https://docs.google.com/document/d/1mI35ySDHd7H55XcEsuavb23hBNMRAuuJiFJMtd-sevg/edit?usp=sharing"
+
+notas_de_atualização = ""
+
+como_usar_link_style = "color: #e5e5e5; font-family: 'Cyrillic'; font-size: 16px;"
+conversor_link = "https://www.convertcsv.com/csv-to-json.htm"
+
+
+versão_style = '''
+            QLabel {
+                font-family: "Cyrillic";   
+                font-size: 13px;      
+                background-color: transparent;
+            }
+        '''
+SelecionarValeUfscGabaritoConvertidoStyle = '''
+            QPushButton {
+                background-color: #ee7867;
+                border-radius: 6px;
+                color: #e7e6e3;
+                font-size:13px;
+            }
+            QPushButton:hover {
+                background-color: #00734a;
+            }
+            QPushButton:pressed {
+                background-color: #f6f7fa;
+            }
+        '''
