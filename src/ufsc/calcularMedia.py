efce1b9e9cee08866821789fbfaead3c6da6b5d5
Gustavo Bernardo Rodrigues <gurodrigues.b@gmail.com>
2024-10-24 15:38:33 -0300

    feat: add csv file generation

--- src/ufsc/calcularMedia.py
+++ src/ufsc/calcularMedia.py
@@ -1,5 +1,5 @@
 import json
-
+import pandas as pd
 def calcular_media_acertos(json_path, main_output_path):
     print("------------------------------------------------------------------------")
     print("Calculando média de acertos de cada disciplina...")
@@ -28,11 +28,14 @@ def calcular_media_acertos(json_path, main_output_path):
         media_prova = round(((soma_acertos[disciplina] / qtd_alunos) + media_prova),2)
         medias_acertos[disciplina] = f"{round(soma_acertos[disciplina] / qtd_alunos,2)}/{len(disciplinas[disciplina])}"
     
-    medias_acertos["Media de acertos"] = media_prova
+    medias_acertos["Media de acertos"] = f"{(str(media_prova))}/40"
     
 
     with open(f'{main_output_path}/media_disciplinas_ufsc.json', 'w', encoding='utf-8') as f:
         json.dump(medias_acertos, f, indent=4, default=str, ensure_ascii=False)
+    
+    df = pd.DataFrame(list(medias_acertos.items()), columns=['Matéria', 'Resultado médio de acertos'])
+    df.to_csv(f"{main_output_path}/media_disciplinas_ufsc.csv", index=False)
     print("Média de acertos calculadas com sucesso!!")
     print("------------------------------------------------------------------------")
     print()
