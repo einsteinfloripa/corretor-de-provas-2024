0e43387d18a55a3c2d6ed70fa52ced94e5e4f1eb
Gustavo Bernardo Rodrigues <gurodrigues.b@gmail.com>
2024-10-24 15:36:07 -0300

    refactor: UFSC partial calculation redone

--- src/ufsc/calcularParcial.py
+++ src/ufsc/calcularParcial.py
@@ -1,7 +1,6 @@
 import json
 import pandas as pd
 
-
 from ..utils import convert_to_int
 
 converter_texto = {
@@ -13,15 +12,15 @@ def calcular_parciais_ufsc(main_path,gabarito_vale):
     print("------------------------------------------------------------------------")
     print("Calculando parciais de acordo com o edital da UFSC...")
     json_path = f"{main_path}/proposicoes_assinaladas_ufsc.json"
-    gabarito_path = gabarito_vale
     parcial_questoes = {}
-    gabarito_df = pd.read_csv(gabarito_path)
+    gabarito_df = pd.read_csv(gabarito_vale,dtype={"Total de proposições": int}, encoding='utf-8')
+    gabarito_df['Total de proposições'] = gabarito_df['Total de proposições'].astype(int)
 
     with open(json_path, 'r') as arquivo:
         dados = json.load(arquivo)
 
-    def aluno_props_lista(resposta_binario, props_correta):
-        respostas = []
+    def aluno_props_lista(resposta_binario, props_correta):#valor em binário, proposições corretas da questão atual
+        respostas = [] #lista de proposições assinaladas pelo estudante
         for resposta in range(len(props_correta)):
             props_correta[resposta] = convert_to_int(props_correta[resposta])
 
@@ -31,55 +30,49 @@ def calcular_parciais_ufsc(main_path,gabarito_vale):
                 valor = 2**((numero*-1)-1)
                 respostas.append(valor)
         incorretas = 0
+        corretas = 0
         for resposta in respostas:
             if resposta not in props_correta:
                 incorretas+=1
-        return incorretas
-
+            else:
+                corretas+=1
+        return incorretas,corretas,respostas
     for estudante in dados:
         estudante_respostas = dados[estudante]["respostas_do_aluno"]
         estudante_nome = dados[estudante]["nome"]
         parcial_questoes[estudante] = {
             "nome":estudante_nome,
             "parcial_aluno":{
-                "Português/Literatura":{},
-                "Matemática":{},
-                "Biologia":{},
-                "História":{},
-                "Geografia":{},
-                "Filosofia/Sociologia":{},
-                "Física":{},
-                "Química":{},
-                "linguagens":{}
             } 
         }
-        pontos_disciplina = {}
 
         for index,row in gabarito_df.iterrows():
             index_string = str(index+1) #index comeca em 0, então tem que somar 1
             lista_de_props_corretas = row["Lista de corretas"].replace(" ", "").split(",")
             disciplina = row["Disciplina"]
-            
-            total_de_proposições = row["Total de proposições"]
+            ntpc = len(lista_de_props_corretas)
+            np = row["Total de proposições"]
             proposições_corretas = row["Proposições corretas"]
-            
             if disciplina == "Inglês":
                 linguagem = dados[estudante]["lingua"]
                 linguagem = converter_texto[linguagem]
+                print("A linguagem do aluno:", linguagem)
                 if linguagem == "Espanhol":
-                    total_de_proposições = row["Total de proposições espanhol"]
+                    np = row["Total de proposições espanhol"]
                     proposições_corretas = row["Proposições corretas espanhol"]
             aluno_props_binario = estudante_respostas[index_string]
             aluno_total_props = int(aluno_props_binario.count("1"))
-            npi = aluno_props_lista(aluno_props_binario,lista_de_props_corretas)
-            if aluno_total_props > npi:
-                pontuacao_questao = ((total_de_proposições - (proposições_corretas - (aluno_total_props - npi)))/total_de_proposições)
+            npi,npc,opcoes_assinaladas_aluno = aluno_props_lista(aluno_props_binario,lista_de_props_corretas)
+            if npc > npi:
+                pontuacao_questao = ((np - (ntpc - (npc - npi)))/np)
             else:
                 pontuacao_questao = 0
             if disciplina=="Inglês" or disciplina=="Espanhol":
                 disciplina = "linguagens"
-            parcial_questoes[estudante]["parcial_aluno"][disciplina][index_string] = round(pontuacao_questao,2)
-
+            if disciplina not in parcial_questoes[estudante]["parcial_aluno"]:
+                parcial_questoes[estudante]["parcial_aluno"][disciplina] = {index_string:round(pontuacao_questao,2)}
+            else:
+                parcial_questoes[estudante]["parcial_aluno"][disciplina][index_string] = round(pontuacao_questao,2)
     with open(f'{main_path}/parcias_ufsc.json', 'w', encoding="utf-8") as f:
         json.dump(parcial_questoes, f, indent=4, default=str, ensure_ascii=False)
     print("Parciais da UFSC foram calculadas com sucesso.")
@@ -89,8 +82,8 @@ def calcular_parciais_ufsc(main_path,gabarito_vale):
     print()
 
 if __name__ == "__main__":
-    calcular_parciais_ufsc("./output/json","assets/spreadsheets/vale gabarito csv.csv" )
+    calcular_parciais_ufsc("./src./output.","./src./assets/spreadsheets./a.csv")
 """
-parciais feitas de acordo com o edital da prova do vestibular da ufsc de 2023
-https://vestibularunificado2024.ufsc.br/files/2023/10/Edital-Vestibular-2024-RETIFICADO.pdf
+parciais feitas de acordo com o edital da provas do vestibular da ufsc de 2024
+https://drive.google.com/file/d/1sUbkCkJ5-XZiPlAB6S72HIkgJC0XoqiX/view?usp=sharing
 """
